c148a71643913a17afd5fd29e6c2b49f7a4aaf0b
Allen Lee <allen93075@gmail.com>
2025-02-27 19:24:12 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+// script.js
+
+// Example: A simple click handler
+const button = document.getElementById("ContactButton");
+
+button.addEventListener("click", function () {
+  // Redirect to the contact page
+  window.location.href = "contact.html";
+});
+
+
