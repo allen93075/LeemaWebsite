a5644cf0e77ba7ee35e0e4d8a039b2f5761b7f71
ShaneChen0219 <hsiangyuan0219@gmail.com>
2025-03-04 16:57:50 -0500

    Added icon

---
+++ leematrading-frontend/src/Components/Footer.jsx
@@ -0,0 +1,18 @@
+const Footer = () => {
+  return (
+    <footer className="bg-[#001F3F] text-[#CD7F32] text-center py-4">
+      <div>
+        <p className="text-xl">&copy; 2025 Leema Trading Co., Ltd</p>
+      </div>
+      <div>
+        <a
+          href="mailto:sandralee@leematrading.com"
+          className="tex-sm hover:underline"
+        >
+          sandralee@leematrading.com
+        </a>
+      </div>
+    </footer>
+  );
+};
+export default Footer;
---
+++ leematrading-frontend/src/Components/Navbar.jsx
@@ -0,0 +1,31 @@
+import Link from "next/link";
+const Navbar = () => {
+  return (
+    <nav className="bg-[#001F3F] text-[#CD7F32] py-4 px-6 flex justify-between items-center fixed w-full top-0 shadow-md z-50">
+      <div className="flex space-x-6">
+        <Link href="/">
+          <span className="text-lg hover:text-white transition-colors cursor-pointer">Home</span>
+        </Link>
+        {/* <Link href="/shop">
+          <span className="text-lg hover:text-white transition-colors cursor-pointer">Shop</span>
+        </Link> */}
+        <Link href="/about">
+          <span className="text-lg hover:text-white transition-colors cursor-pointer">About Us</span>
+        </Link>
+        <Link href="/contact">
+          <span className="text-lg hover:text-white transition-colors cursor-pointer">Contact</span>
+        </Link>
+      </div>
+      {/* <div className="flex space-x-4">
+        <Link href="/account">
+          <span className="cursor-pointer hover:text-white">🔑</span>
+        </Link>
+        <Link href="/cart">
+          <span className="cursor-pointer hover:text-white">🛒 (0)</span>
+        </Link>
+      </div> */}
+    </nav>
+  );
+};
+
+export default Navbar;
