4a74ae7b77114f7547b1d5dee94c05206ab03966
Allen Lee <allen93075@gmail.com>
2025-03-11 19:42:06 -0400

    update shell commend

--- scripts/start_service.sh
+++ scripts/start_service.sh
@@ -2,4 +2,4 @@
 echo "Changing directory to application folder..."
 cd /home/ubuntu/LeemaWebsite/leematrading-frontend || exit 1
 echo "Starting the application using npm..."
-sudo npm run start
+sudo systemctl start leematrading-frontend
--- scripts/stop_service.sh
+++ scripts/stop_service.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo "Stopping the systemd service..."
-sudo fuser -k 80/tcp || echo "No process is using port 80."
-sudo pkill -f "npm run start" || echo "No process is running npm run start."
+sudo systemctl stop leematrading-frontend
 echo "Service stopped successfully."
 echo "Stopping the application..."
