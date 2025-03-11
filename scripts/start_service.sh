448385e96157438ad19730ceb5a2e731a5e843ef
Allen Lee <allen93075@gmail.com>
2025-03-11 17:34:46 -0400

    add script

--- scripts/start_service.sh
+++ scripts/start_service.sh
@@ -2,4 +2,4 @@
 echo "Changing directory to application folder..."
 cd /home/ubuntu/LeemaWebsite/leematrading-frontend || exit 1
 echo "Starting the application using npm..."
-pm2 start npm --name "leematrading" -- run start
+sudo npm run start
