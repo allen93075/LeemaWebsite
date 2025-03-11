149dbb9afdae515826b7209f37cf465c12d4d442
Allen Lee <allen93075@gmail.com>
2025-03-11 18:24:37 -0400

    update stop service shell

--- scripts/stop_service.sh
+++ scripts/stop_service.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 echo "Stopping the systemd service..."
-sudo pkill -f "npm run start" || echo "Service not running."
+sudo fuser -k 80/tcp || echo "No process is using port 80."
+sudo pkill -f "npm run start" || echo "No process is running npm run start."
+echo "Service stopped successfully."
+echo "Stopping the application..."
